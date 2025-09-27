64b215c7ea468121c53ed4fdb9b54b8fe5aa1cf0
Amy Comyns Haugh <comynsamy03@gmail.com>
2025-09-27 10:57:37 +0100

    Movie card component created

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import {Grid} from "@mui/material";
+import MovieCard from "../components/movieCard";
 
 
 
 const HomePage = (props) => {
   const movies = props.movies;
-    
-  return (
-    
-   <Grid container>
+
+   return (
+  <Grid container>
       <Grid size={12}>
           <h1> HomePage </h1>
       </Grid>
-    </Grid> 
-    
+      <Grid size={3}>
+        <MovieCard movie={movies[0]} />
+      </Grid>
+    </Grid>
   );
 };
 export default HomePage;
