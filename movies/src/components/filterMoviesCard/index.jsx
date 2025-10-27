0cee2f37e2db4a797312edea46043fa807051368
Amy Comyns Haugh <comynsamy03@gmail.com>
2025-10-27 19:20:08 +0000

    Edited filters

--- movies/src/components/filterMoviesCard/index.jsx
+++ movies/src/components/filterMoviesCard/index.jsx
@@ -3,17 +3,14 @@ import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
-import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
 import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
 import img from '../images/pexels-dziana-hasanbekava-5480827.jpg'
 import { getGenres } from "../../api/tmdb-api";
 import { useQuery } from '@tanstack/react-query';
 import Spinner from '../spinner';
-
 
 const formControl = 
   {
@@ -36,6 +33,7 @@ export default function FilterMoviesCard(props) {
   if (isError) {
     return <h1>{error.message}</h1>;
   }
+
   const genres = data.genres;
   if (genres[0].name !== "All"){
     genres.unshift({ id: "0", name: "All" });
@@ -46,98 +44,78 @@ export default function FilterMoviesCard(props) {
     props.onUserInput(type, value); 
   };
 
-  const handleTextChange = (e, props) => {
-    handleChange(e, "name", e.target.value);
-  };
-
-  const handleGenreChange = (e) => {
-    handleChange(e, "genre", e.target.value);
-  };
-
-
-
   return (
     <Card 
       sx={{
         backgroundColor: "rgb(204, 204, 0)"
       }} 
-      variant="outlined">
+      variant="outlined"
+    >
       <CardContent>
         <Typography variant="h5" component="h1">
           <SearchIcon fontSize="large" />
           Filter the movies.
         </Typography>
-            <TextField
-      sx={{...formControl}}
-      id="filled-search"
-      label="Search field"
-      type="search"
-      variant="filled"
-      value={props.titleFilter}
-      onChange={handleTextChange}
-    />
 
-        <FormControl sx={{...formControl}}>
-          <InputLabel id="genre-label">Genre</InputLabel>
-            <Select
-    labelId="genre-label"
-    id="genre-select"
-    defaultValue=""
-    value={props.genreFilter}
-    onChange={handleGenreChange}
-  >
-
-            {genres.map((genre) => {
-              return (
-                <MenuItem key={genre.id} value={genre.id}>
-                  {genre.name}
-                </MenuItem>
-              );
-            })}
-          </Select>
+        <FormControl sx={{ ...formControl }}>
+          <TextField
+            label="Search"
+            variant="filled"
+            type="search"
+            value={props.titleFilter}
+            onChange={(e) => handleChange(e, "name", e.target.value)}
+          />
         </FormControl>
 
         <FormControl sx={{ ...formControl }}>
-         <InputLabel id="rating-label">Minimum Rating</InputLabel>
-         <Select
-        labelId="rating-label"
-        id="rating-select"
-        value={props.ratingFilter}
-        onChange={(e) => handleChange(e, "rating", e.target.value)}
-        >
-        <MenuItem value="0">All</MenuItem>
-        <MenuItem value="5">5+</MenuItem>
-        <MenuItem value="6">6+</MenuItem>
-        <MenuItem value="7">7+</MenuItem>
-        <MenuItem value="8">8+</MenuItem>
-        </Select>
+          <TextField
+            select
+            label="Genre"
+            variant="filled"
+            value={props.genreFilter}
+            onChange={(e) => handleChange(e, "genre", e.target.value)}
+          >
+            {genres.map((genre) => (
+              <MenuItem key={genre.id} value={genre.id}>
+                {genre.name}
+              </MenuItem>
+            ))}
+          </TextField>
         </FormControl>
 
         <FormControl sx={{ ...formControl }}>
-        <TextField
-        id="year-filter"
-        label="Release Year"
-        variant="filled"
-        value={props.yearFilter}
-        onChange={(e) => handleChange(e, "year", e.target.value)}
-        placeholder="e.g. 2023"
-        />
+          <TextField
+            select
+            label="Minimum Rating"
+            variant="filled"
+            value={props.ratingFilter}
+            onChange={(e) => handleChange(e, "rating", e.target.value)}
+          >
+            {["0","5","6","7","8"].map((value) => (
+              <MenuItem key={value} value={value}>
+                {value === "0" ? "All" : `${value}+`}
+              </MenuItem>
+            ))}
+          </TextField>
         </FormControl>
 
+        <FormControl sx={{ ...formControl }}>
+          <TextField
+            label="Release Year"
+            variant="filled"
+            value={props.yearFilter}
+            onChange={(e) => handleChange(e, "year", e.target.value)}
+            placeholder="e.g. 2023"
+          />
+        </FormControl>
 
       </CardContent>
+
       <CardMedia
         sx={{ height: 300 }}
         image={img}
         title="Filter"
       />
-      <CardContent>
-        <Typography variant="h5" component="h1">
-          <SearchIcon fontSize="large" />
-          Filter the movies.
-          <br />
-        </Typography>
-      </CardContent>
     </Card>
   );
 }
