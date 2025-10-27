44885a337f3ec2843209de8d9a2ad9262852e873
Amy Comyns Haugh <comynsamy03@gmail.com>
2025-10-27 21:11:51 +0000

    Movie Card Styling added

--- movies/src/components/movieCard/index.jsx
+++ movies/src/components/movieCard/index.jsx
@@ -4,89 +4,103 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import CardHeader from "@mui/material/CardHeader";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
-import DeleteIcon from '@mui/icons-material/Delete'; // trash bin icon
+import DeleteIcon from '@mui/icons-material/Delete';
 import CalendarIcon from "@mui/icons-material/CalendarTodayTwoTone";
 import StarRateIcon from "@mui/icons-material/StarRate";
 import IconButton from "@mui/material/IconButton";
 import { Grid } from "@mui/material";
-import img from "../images/film-poster-placeholder.png";
 import { Link } from "react-router";
-import Avatar from '@mui/material/Avatar';
+import img from "../images/film-poster-placeholder.png";
 
 export default function MovieCard({ movie, action, isPlaylistPage = false }) {
-   const { favorites, addToFavorites, playlist, addToPlaylist, removeFromPlaylist } = useContext(MoviesContext);
-   const isFavorite = favorites.includes(movie.id);
-   const isInPlaylist = playlist.find((m) => m.id === movie.id);
+  const { favorites, addToFavorites, playlist, addToPlaylist, removeFromPlaylist } = useContext(MoviesContext);
+  const isFavorite = favorites.includes(movie.id);
+  const isInPlaylist = playlist.find((m) => m.id === movie.id);
 
-   const handleAddToFavorite = (e) => {
-     e.preventDefault();
-     addToFavorites(movie);
-   };
+  const handlePlaylistAction = (e) => {
+    e.preventDefault();
+    if (isPlaylistPage) removeFromPlaylist(movie);
+    else isInPlaylist ? removeFromPlaylist(movie) : addToPlaylist(movie);
+  };
 
-   const handlePlaylistAction = (e) => {
-     e.preventDefault();
-     if (isPlaylistPage) {
-       removeFromPlaylist(movie);
-     } else {
-       isInPlaylist ? removeFromPlaylist(movie) : addToPlaylist(movie);
-     }
-   };
+  return (
+    <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+      <Card
+        sx={{
+          width: "100%",
+          maxWidth: 300,
+          height: 500,
+          m: 1,
+          boxShadow: 3,
+          transition: "transform 0.3s, box-shadow 0.3s",
+          "&:hover": { transform: "translateY(-10px)", boxShadow: 8 },
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between"
+        }}
+      >
+        <CardMedia
+          component="img"
+          sx={{ height: 350 }}
+          image={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : img}
+          alt={movie.title}
+        />
 
-   return (
-     <Card>
-       <CardHeader
-         avatar={
-           isFavorite ? (
-             <Avatar sx={{ backgroundColor: 'red' }}>
-               <FavoriteIcon />
-             </Avatar>
-           ) : null
-         }
-         title={
-           <Typography variant="h5" component="p">
-             {movie.title}{" "}
-           </Typography>
-         }
-       />
+        <CardContent sx={{ flexGrow: 1, textAlign: "center" }}>
+          <Typography
+            variant="h6"
+            component="p"
+            sx={{
+              fontFamily: "'Playfair Display', serif",
+              fontStyle: "italic",
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              textOverflow: "ellipsis"
+            }}
+            title={movie.title}
+          >
+            {movie.title}
+          </Typography>
 
-       <CardMedia
-         sx={{ height: 500 }}
-         image={
-           movie.poster_path
-             ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-             : img
-         }
-       />
-       <CardContent>
-         <Grid container>
-           <Grid size={{ xs: 6 }}>
-             <Typography variant="h6" component="p">
-               <CalendarIcon fontSize="small" /> {movie.release_date}
-             </Typography>
-           </Grid>
-           <Grid size={{ xs: 6 }}>
-             <Typography variant="h6" component="p">
-               <StarRateIcon fontSize="small" /> {movie.vote_average}
-             </Typography>
-           </Grid>
-         </Grid>
-       </CardContent>
-       <CardActions disableSpacing>
-         {action && action(movie)}
-         <IconButton onClick={handlePlaylistAction}>
-           {isPlaylistPage ? <DeleteIcon color="error" /> : <PlaylistAddIcon color={isInPlaylist ? "primary" : "default"} />}
-         </IconButton>
-         <Link to={`/movies/${movie.id}`}>
-           <Button variant="outlined" size="medium" color="primary">
-             More Info ...
-           </Button>
-         </Link>
-       </CardActions>
-     </Card>
-   );
+          <Grid container spacing={1} justifyContent="center" sx={{ mt: 1 }}>
+            <Grid item sx={{ display: "flex", alignItems: "center" }}>
+              <CalendarIcon fontSize="small" sx={{ mr: 0.5 }} />
+              <Typography variant="subtitle2">{movie.release_date}</Typography>
+            </Grid>
+            <Grid item sx={{ display: "flex", alignItems: "center" }}>
+              <StarRateIcon fontSize="small" sx={{ mr: 0.5, color: "#fbc02d" }} />
+              <Typography variant="subtitle2">{movie.vote_average.toFixed(1)}</Typography>
+            </Grid>
+          </Grid>
+        </CardContent>
+
+        <CardActions sx={{ justifyContent: "space-between" }}>
+          {action && action(movie)}
+          <IconButton sx={{ "&:hover": { transform: "scale(1.2)", transition: "0.2s" } }} onClick={handlePlaylistAction}>
+            {isPlaylistPage ? <DeleteIcon color="error" /> : <PlaylistAddIcon color={isInPlaylist ? "primary" : "default"} />}
+          </IconButton>
+          <Link to={`/movies/${movie.id}`} style={{ textDecoration: "none" }}>
+            <Button
+              variant="contained"
+              size="medium"
+              sx={{
+                background: "linear-gradient(90deg, #6ec1e4, #00aaff)",
+                color: "white",
+                "&:hover": {
+                  background: "linear-gradient(90deg, #00aaff, #6ec1e4)",
+                  transform: "scale(1.05)"
+                }
+              }}
+            >
+              More Info
+            </Button>
+          </Link>
+        </CardActions>
+      </Card>
+    </Grid>
+  );
 }
