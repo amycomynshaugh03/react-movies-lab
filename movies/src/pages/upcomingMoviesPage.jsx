5adc1f8ac6d06e788afbb10884092464ef17a7c0
Amy Comyns Haugh <comynsamy03@gmail.com>
2025-10-28 13:08:33 +0000

    Pagnitation added to upComing Movies Page

--- movies/src/pages/upcomingMoviesPage.jsx
+++ movies/src/pages/upcomingMoviesPage.jsx
@@ -1,18 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import PageTemplate from "../components/templateMovieListPage";
 import { getUpcomingMovies } from "../api/tmdb-api";
 import Spinner from "../components/spinner";
 import IconButton from "@mui/material/IconButton";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
 import { MoviesContext } from "../contexts/moviesContext";
-import { useContext } from "react";
-
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
 const UpcomingMoviesPage = () => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
-  const { addToMustWatch } = useContext(MoviesContext);
+  const [page, setPage] = useState(1);
+  const moviesPerPage = 10;
 
+  const { addToMustWatch } = useContext(MoviesContext);
 
   useEffect(() => {
     getUpcomingMovies().then((movies) => {
@@ -23,20 +25,42 @@ const UpcomingMoviesPage = () => {
 
   if (loading) return <Spinner />;
 
- const handleAddToPlayList = (movie) => (
-  <IconButton onClick={() => addToMustWatch(movie)}>
-    <PlaylistAddIcon color="primary" />
-  </IconButton>
-);
+  const handleAddToPlayList = (movie) => (
+    <IconButton onClick={() => addToMustWatch(movie)}>
+      <PlaylistAddIcon color="primary" />
+    </IconButton>
+  );
 
+  const pageCount = Math.ceil(movies.length / moviesPerPage);
+
+  const displayedMovies = movies.slice(
+    (page - 1) * moviesPerPage,
+    page * moviesPerPage
+  );
+
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
 
   return (
-    <PageTemplate
-      title="Upcoming Movies"
-      movies={movies}
-      action={handleAddToPlayList}
-      showPlaylistButton={false}  
-    />
+    <>
+      <PageTemplate
+        title="Upcoming Movies"
+        movies={displayedMovies}
+        action={handleAddToPlayList}
+        showPlaylistButton={false}
+      />
+      {pageCount > 1 && (
+        <Stack spacing={2} alignItems="center" sx={{ mt: 2 }}>
+          <Pagination
+            count={pageCount}
+            page={page}
+            onChange={handleChange}
+            color="primary"
+          />
+        </Stack>
+      )}
+    </>
   );
 };
 
